ee79c178ee34c6d08a0d5a5bbda69c3415e45f68
Jerald Sayson <saysonjerald@gmail.com>
2022-09-03 13:52:47 +0800

    Removed: Reverting back to normal

--- graphql-basics/src/myModule.js
+++
@@ -1,9 +0,0 @@
-const meesage = "Hello myname";
-const name = "Jerald Sayson";
-const address = "Philippnies";
-
-const getGreeting = (name) => {
-    return `Hello ${name}, Welcome the club!`
-}
-
-export {meesage, name, getGreeting, address as default}
