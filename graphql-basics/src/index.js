4d639e99e3eedbe0c8a6121012728dd2375fa683
Jerald Sayson <saysonjerald@gmail.com>
2022-09-03 18:44:35 +0800

    Ralational Data: Basics

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -5,25 +5,25 @@ import {v4 as randomID} from 'uuid'
 
 const usersData = [
     {
-        id: randomID(),
+        id: '1',
         name: 'Jerald Sayson',
         email: 'example@gmail.com',
         age: 27,
     },
     {
-        id: randomID(),
+        id: '2',
         name: 'Ian Nogas',
         email: 'example@gmail.com',
         age: 21,
     },
     {
-        id: randomID(),
+        id: '3',
         name: 'Eugene Mosqueda',
         email: 'example@gmail.com',
         age: 26,
     },
     {
-        id: randomID(),
+        id: '4',
         name: 'Roberto Dallas',
         email: 'example@gmail.com',
         age: 72,
@@ -36,19 +36,22 @@ const postData = [
         id: randomID(),
         title: 'Art of War',
         body: 'This is the introduction of the art of war',
-        published: "06-02-1934"
+        published: "06-02-1934",
+        author: '1'
     },
     {
         id: randomID(),
         title: 'The Lorem Ipsum',
         body:  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus consequuntur ut, eaque veritatis tenetur expedita facilis necessitatibus odio nulla quam.",
-        published: "07-02-1943"
+        published: "07-02-1943",
+        author: '2'
     },
     {
         id: randomID(),
         title: 'The Read Power of Low',
         body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, accusamus!",
-        published: "12-02-2014"
+        published: "12-02-2014",
+        author: '3'
     }
 ]
 
@@ -72,6 +75,7 @@ const typeDefs = `
         title: String!
         body: String!
         published: String!
+        author: User!
     }
 `
 
@@ -103,7 +107,7 @@ const resolvers = {
                 id: randomID(),
                 title: 'The Art of War',
                 body: 'This is the introduction of the Art of War by Tsu-chan',
-                published: 2008
+                published: 2008,
             }
         },
         user: () => {
@@ -113,6 +117,13 @@ const resolvers = {
                 email: 'example@gmail.com',
                 age: 34
             }
+        }
+    },
+    Post: {
+        author: (parent) => {
+            return usersData.find((user) => {
+                return user.id === parent.author;
+            })
         }
     }
 }
