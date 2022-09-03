81b0511cc4d6ae403d38f7dfe41db87c2a72d97d
Jerald Sayson <saysonjerald@gmail.com>
2022-09-03 13:41:06 +0800

    Added importing and exporting files

---
+++ graphql-basics/src/index.js
@@ -0,0 +1,10 @@
+// import MyCurrentLocation, {name, meesage, getGreeting} from './myModule';
+// console.log({MyCurrentLocation, name, meesage});
+// console.log(getGreeting(name));
+
+import add, { subtract } from "./math";
+
+const sum = add(1,1);
+const diff = subtract(1,1);
+
+console.log({sum,diff});
---
+++ graphql-basics/src/myModule.js
@@ -0,0 +1,9 @@
+const meesage = "Hello myname";
+const name = "Jerald Sayson";
+const address = "Philippnies";
+
+const getGreeting = (name) => {
+    return `Hello ${name}, Welcome the club!`
+}
+
+export {meesage, name, getGreeting, address as default}
